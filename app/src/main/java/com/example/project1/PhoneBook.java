045a08334fe3e963c6fbf127afab45cf3ac0cad4
nkeum <rmaskdus0208@gmail.com>
2021-07-01 22:53:58 +0900

    profile tab added
    to do
    - click and link gallery, change photo
    - set onclicklistener
    - design recyclerview

---
+++ app/src/main/java/com/example/project1/PhoneBook.java
@@ -0,0 +1,30 @@
+package com.example.project1;
+
+public class PhoneBook {
+    private int image;
+    private String name;
+    private String num;
+
+    PhoneBook(Integer image, String name, String num)
+    {
+        this.image = image;
+        this.name = name;
+        this.num = num;
+    }
+
+    public int getImage(){return image;}
+    public String getName(){return name;}
+    public String getNum(){return num;}
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
